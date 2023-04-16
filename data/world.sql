6c3f1247dba19860f2006a74f769e95b538377c6
Ddang168 <124846312+Ddang168@users.noreply.github.com>
2023-04-16 15:03:59 +0100

    Updated

--- data/world.sql
+++ data/world.sql
@@ -57,7 +57,7 @@ INSERT INTO `city` VALUES (3,'Herat','AFG','Herat',186800);
 INSERT INTO `city` VALUES (4,'Mazar-e-Sharif','AFG','Balkh',127800);
 INSERT INTO `city` VALUES (5,'Amsterdam','NLD','Noord-Holland',731200);
 INSERT INTO `city` VALUES (6,'Rotterdam','NLD','Zuid-Holland',593321);
-INSERT INTO `city` VALUES (7,'Haag','NLD','Zuid-Holland',440900);
+INSERT INTO `city` VALUES (7,'Haag','NLD','Zuid-Holland',514861);
 INSERT INTO `city` VALUES (8,'Utrecht','NLD','Utrecht',234323);
 INSERT INTO `city` VALUES (9,'Eindhoven','NLD','Noord-Brabant',201843);
 INSERT INTO `city` VALUES (10,'Tilburg','NLD','Noord-Brabant',193238);
